4ef2b39323872d488cdf53a5885ce7f25a6f4b07
Squix <paul.morizot@utt.fr>
2021-10-15 15:02:30 +0200

    ajout du contract au dist

--- build.ts
+++ build.ts
@@ -22,6 +22,8 @@ logger.timestamp = false;
         await copy('./src/views', './dist/views');
         // Copy production env file
         await copy('./src/pre-start/env/production.env', './dist/pre-start/env/production.env');
+        //copy contract files
+        await copy('./src/nft/contract.tz', './dist/nft/contract.tz');
         // Copy back-end files
         await exec('tsc --build tsconfig.prod.json', './')
     } catch (err) {
