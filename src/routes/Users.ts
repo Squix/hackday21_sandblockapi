01c7d33f069aee14de4677644d74aaa4c60566d5
Squix <paul.morizot@utt.fr>
2021-10-15 14:32:54 +0200

    on desactive tout ça

--- src/routes/Users.ts
+++ src/routes/Users.ts
@@ -1,10 +1,10 @@
 import StatusCodes from 'http-status-codes';
 import { Request, Response } from 'express';
 
-import UserDao from '@daos/User/UserDao.mock';
+
 import { paramMissingError } from '@shared/constants';
 
-const userDao = new UserDao();
+const userDao = <any>{};
 const { BAD_REQUEST, CREATED, OK } = StatusCodes;
 
 
